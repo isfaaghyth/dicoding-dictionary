879a9ba088d970a2ce8fd6dbea9beb00d22ad3f2
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-07-28 03:49:17 +0700

    add floatinSearchView

--- app/src/main/java/app/isfaaghyth/dictionary/ui/MainActivity.java
+++ app/src/main/java/app/isfaaghyth/dictionary/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package app.isfaaghyth.dictionary;
+package app.isfaaghyth.dictionary.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import app.isfaaghyth.dictionary.R;
 
 public class MainActivity extends AppCompatActivity {
 
